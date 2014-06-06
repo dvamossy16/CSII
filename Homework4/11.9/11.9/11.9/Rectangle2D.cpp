b7c4ae358a4d985831207dd2b01729084e4bdd6d
dvamossy16 <dvamossy16@my.whitworth.edu>
2014-06-06 10:51:02 -0700

    rectangle
    ss

--- Homework4/11.9/11.9/11.9/Rectangle2D.cpp
+++ Homework4/11.9/11.9/11.9/Rectangle2D.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 using namespace std;
 
-
-//******************constructor1******************
+//First Constructor
 Rectangle2D::Rectangle2D()
 {
  x = 0;
@@ -11,8 +10,7 @@ Rectangle2D::Rectangle2D()
  width = 1;
  height = 1;
 }
-//******************constructor2******************
-// A new rectanglw with new co=ordinates, height and width is created.
+//Second Constructor // A new rectangle with new coordinates, height and width is created.
 Rectangle2D::Rectangle2D(double newX, double newY, double newWidth, double newHeight)
 {
  x = newX;
@@ -20,52 +18,52 @@ Rectangle2D::Rectangle2D(double newX, double newY, double newWidth, double newHe
  width = newWidth;
  height = newHeight;
 }
-// used to get the co-ordinate x 
+//Get Function for X
 double Rectangle2D::getX() const
 {
  return x;
 }
-// used to get the co-ordinate y
+//Get Function for Y
 double Rectangle2D::getY() const
 {
  return y;
 }
-// used to get the width of rectangle
+//Get Function for Width
 double Rectangle2D::getWidth() const
 {
  return width;
 }
-// used to get the height of rectangle
+//Get Function for Height
 double Rectangle2D::getHeight() const
 {
  return height;
 }
-// used to set the x co-ordinate that is given by user as the new x co-ordinate
+//Set Function for X
 void Rectangle2D::setX(double newX)
 {
  x = newX;
 }
-// used to set the y co-ordinate that is given by user as the new y co-ordinate
+//Set Function for Y
 void Rectangle2D::setY(double newY)
 {
  y = newY;
 }
-// used to set the width that is given by user as the new width
+//Set a new width
 void Rectangle2D::setWidth(double newWidth)
 {
  width = newWidth;
 }
-// used to set the height that is given by user as the new height
+//Set a new height
 void Rectangle2D::setHeight(double newHeight)
 {
  height = newHeight;
 }
-// used to give the area of the rectangle created
+//Get the Area based on parameters
 double Rectangle2D::getArea()
 {
  return width * height;
 }
-// used to give the perimeter of the rectangle created
+//Get the Perimeter based on parameters
 double Rectangle2D::getPerimeter()
 {
  return 2 * (width + height);
@@ -73,13 +71,13 @@ double Rectangle2D::getPerimeter()
 // to check if the given rectangle contains the point x, y inside it or not
 bool Rectangle2D::contains(double x, double y)
 {
- // if the co-ordinate x is less than or equal to x+width/2 and greater than or equal to x-width/2
+
  if(x <= this->x + width / 2 && x >= this->x - width / 2)
-  // if the co-ordinate y is less than or equal to y+width/2 and greater than or equal to y-width/2
+
   if(y <= this->y + height / 2 && y >= this->y - height / 2)
    // it returns true if the above statements are satisfied
    return true;
- // else it returns false
+ 
  return false;
 }
 // to check if the given rectangle contains the other rectangle inside it or not
