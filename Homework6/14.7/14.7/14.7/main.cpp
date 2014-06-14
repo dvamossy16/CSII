01d7a6c9e0d61cb536b9ca594cbf63108581d08d
dvamossy16 <dvamossy16@my.whitworth.edu>
2014-06-13 17:12:19 -0700

    change
    change

--- Homework6/14.7/14.7/14.7/main.cpp
+++ Homework6/14.7/14.7/14.7/main.cpp
@@ -3,22 +3,14 @@
 using namespace std;
 
 int main(){
- cout << "Enter Real and imaginary parts of z1 followed by space" << endl;
+ cout << "Enter the first complex number" << endl;
  double r1,i1,r2,i2;
  cin >> r1 >> i1;
  Complex a(r1,i1);
- cout << "Do you want to print it? Y for yes, N for No. "; char asd; cin >> asd;
- if (asd == 'Y'){
-  a.print();
- }
- cout << "Enter Real and imaginary parts of z2 followed by space" << endl;
+  cout << "Enter the second complex number" << endl;
  cin >> r2 >> i2;
  Complex b(r2,i2),c(a.add(b));
- cout << "Do you want to print it? Y for yes, N for No. "; cin >> asd;
- if (asd == 'Y'){
-  b.print();
- }
- cout << "\nSUM: \t\t(" << c.getReal() << ") + (" << c.getImag() << ")i" << endl;
+  cout << "\nSUM: \t\t(" << c.getReal() << ") + (" << c.getImag() << ")i" << endl;
  
  reset(r1,i1,a,c);
  c = a.sub(b);
